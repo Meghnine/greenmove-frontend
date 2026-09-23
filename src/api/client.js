3e5447f867ff828222ccfb122b388133567ddb8d
Nadiya <nadiameghnine0102@gmail.com>
2026-09-23 09:01:53 +0200

    Update backend API URL for Azure deployment

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://127.0.0.1:8000";
+export const API_BASE_URL =
+  "https://greenmove-backend.icysky-194f5e0b.francecentral.azurecontainerapps.io";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
